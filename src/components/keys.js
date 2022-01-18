68f4dcaf558d9461af471d74a42eb0593c2bf354
AnubhaSingh01 <bhartianubha9@gmail.com>
2022-01-18 21:16:34 +0530

    added all code

---
+++ src/components/keys.js
@@ -0,0 +1,17 @@
+/* The Complete documentation 👇👇👇👇 */
+/* https://developers.google.com/custom-search/v1/using_rest */
+
+
+/* Get your api key 👇👇👇👇 */
+/* https://developers.google.com/custom-search/v1/overview#api_key*/
+
+const API_KEY = "AIzaSyCqIzW5nPrmb4Rgb1cY2gvArqI2ZYm0FOk";
+
+/* Get your   Search engine ID and place in CONTEXT_KEY*/ 
+/* https://cse.google.co.in/cse/all */
+const CONTEXT_KEY = "c3da70e3eb34a3b57";
+
+
+const baseURL = "https://www.googleapis.com/customsearch/v1";
+
+export {API_KEY, CONTEXT_KEY, baseURL};
