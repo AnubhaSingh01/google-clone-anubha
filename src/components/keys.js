6cb8f26c177cfa67d5839919a6029d1e94dbbe1b
AnubhaSingh01 <bhartianubha9@gmail.com>
2022-01-18 21:18:20 +0530

    removed my key

--- src/components/keys.js
+++ src/components/keys.js
@@ -5,13 +5,13 @@
 /* Get your api key 👇👇👇👇 */
 /* https://developers.google.com/custom-search/v1/overview#api_key*/
 
-const API_KEY = "AIzaSyCqIzW5nPrmb4Rgb1cY2gvArqI2ZYm0FOk";
+const API_KEY = "API_KEY";
 
 /* Get your   Search engine ID and place in CONTEXT_KEY*/ 
 /* https://cse.google.co.in/cse/all */
 const CONTEXT_KEY = "c3da70e3eb34a3b57";
 
 
-const baseURL = "https://www.googleapis.com/customsearch/v1";
+const baseURL = "CONTEXT_KEY";
 
 export {API_KEY, CONTEXT_KEY, baseURL};
